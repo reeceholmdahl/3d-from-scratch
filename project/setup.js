8572288debffb363879bdcf13a93e27d6cefb7d1
Reece Holmdahl <3149587+reeceholmdahl@users.noreply.github.com>
2021-06-30 19:13:11 -0500

    still working on setup script

--- project/setup.js
+++ project/setup.js
@@ -1,14 +1,15 @@
 const fs = require('fs'),
     path = require('path'),
     childProcess = require('child_process');
+const { stderr } = require('process');
 
     
 function setupProject() {
-    childProcess.execSync('npm install')
-
     fs.mkdir(path.join(__dirname, '../out'), (err) => {
         console.error(`Could not set up project: ${err.message}`)
     });
+
+    childProcess.exec('npm install').stderr.pipe(stderr);
 }
 
 setupProject();
